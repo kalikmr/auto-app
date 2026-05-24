0f2a4b5b2ac78ebb01a6ab9d9936d0d26edaeb35
sahil lone <sahilcomputers000@gmail.com>
2026-05-24 03:21:33 -0700

    fix mongodb connection

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/auto")
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
   console.log("MongoDB Connected ✅");
 })
